74c5c4ce0d2590471f177c1390ebbec5e937ae62
Edward <emma319993@gmail.com>
2025-04-30 14:24:57 +0800

    Inference code (WIP)

--- kernel_gpu.py
+++ kernel_gpu.py
@@ -333,5 +333,16 @@ def main():
     #save_velocity_field_vtk(ux, uy, filename="velocity_field.vtk")
     #save_velocity_field_vti(ux, uy, filename="velocity_field.vti")
 
+def threads_per_block(dim_x=16, dim_y=16):
+    """Returns standard 2D thread block dimensions."""
+    # Common choice, matching LBMSolverD2Q9GPU.blockdim
+    return (dim_x, dim_y)
+
+def blocks_per_grid(nx, ny, block_dim_x=16, block_dim_y=16):
+    """Calculates the grid dimensions needed for a given nx, ny and block size."""
+    grid_dim_x = (nx + block_dim_x - 1) // block_dim_x
+    grid_dim_y = (ny + block_dim_y - 1) // block_dim_y
+    return (grid_dim_x, grid_dim_y)
+
 if __name__ == "__main__":
     main()
